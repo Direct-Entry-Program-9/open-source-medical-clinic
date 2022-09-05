7d5801f6d0be11301a5b022ea178165fd62422e8
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-09-05 17:12:51 +0530

    Add Settings UI

---
+++ src/main/java/lk/ijse/dep9/clinic/controller/SettingsFormController.java
@@ -0,0 +1,30 @@
+package lk.ijse.dep9.clinic.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class SettingsFormController {
+    public AnchorPane pneContainer;
+    public Button btnAbout;
+    public Button btnPassword;
+    public Button btnManageFields;
+    public Button btnDiscount;
+    public Button btnHospitalFee;
+
+    public void initialize(){
+
+    }
+
+    public void btnHospitalFeeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDiscountOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPasswordOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAboutOnAction(ActionEvent actionEvent) {
+    }
+}
