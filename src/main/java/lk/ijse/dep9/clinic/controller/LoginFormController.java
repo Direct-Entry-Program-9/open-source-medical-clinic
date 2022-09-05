44c26ad72b3583fefd723ba98a557f0e6fe8ade6
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2022-09-05 13:31:30 +0530

    Close #5

--- src/main/java/lk/ijse/dep9/clinic/controller/LoginFormController.java
+++ src/main/java/lk/ijse/dep9/clinic/controller/LoginFormController.java
@@ -50,11 +50,12 @@ public class LoginFormController {
         Class.forName("com.mysql.cj.jdbc.Driver");
         try(Connection connection = DriverManager.
                 getConnection("jdbc:mysql://localhost:3306/medical_clinic", "root", "mysql")){
-            String sql = "SELECT role FROM User WHERE username='%s' AND password='%s'";
-            sql = String.format(sql, username, password);
 
-            Statement stm = connection.createStatement();
-            ResultSet rst = stm.executeQuery(sql);
+            String sql = "SELECT role FROM User WHERE username=? AND password=?";
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setString(1, username);
+            stm.setString(2, password);
+            ResultSet rst = stm.executeQuery();
 
             if (rst.next()){
                 String role = rst.getString("role");
